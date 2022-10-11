eb73e4d5af21c54a61e0e2214e8ac8a3f21a5911
Safin-Ali <safin.ali.7205@gmail.com>
2022-10-11 12:35:56 +0600

    radio button true false validation code writen but fully not wrote!

--- src/components/main/quiz-page/QuizCard.js
+++ src/components/main/quiz-page/QuizCard.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import { CheckIcon } from '@heroicons/react/24/solid';
+import React, { useState } from 'react';
 import "./quiz-card.css";
 
-function Questions ({option,correctAnswer,idx}) {
+function Questions ({option,correctAnswer}) {
+    const choose = 'choose';
+    const [bool,setBool] = useState([]);
+    const randomID = Math.random()*100+1;
+
+    function handleQuizAnswer () {
+        const compare = correctAnswer === option ? true : false;
+        setBool(compare)
+    }
+
+
     return(
-        <div className='border border-2'>
-            <div className='p-5 flex justify-between items-center'>
-            <input value={correctAnswer} id={idx} type="radio"/>
-            <label className='font-mona' htmlFor={idx}>{option}</label>
+        <div className={`border border-2 rounded-md ${bool === true && 'border-green-600'} ${bool || 'border-red-600'}`}>
+            <div className='p-5 flex justify-center items-center'>
+            <div><input onClick={handleQuizAnswer} value={correctAnswer} className='mr-2' name={choose} id={randomID} type="radio"/></div>
+            <div>
+            <label onClick={handleQuizAnswer} className='cursor-pointer font-mona text-pink-600' name={choose} htmlFor={randomID}>{option}</label>
+            </div>
             </div>
         </div>
     )
@@ -21,7 +34,7 @@ const QuizCard = ({data,index}) => {
             <p className='font-mona'>Quiz: {index} {question}</p>
             <div className="mt-10 pb-10 grid gap-10 grid-cols-2">
                 {
-                   options.map((option,idx) =>  <Questions id={idx} key={idx} correctAnswer={correctAnswer} option={option}></Questions>)
+                   options.map((option,idx) =>  <Questions key={idx} correctAnswer={correctAnswer} option={option}></Questions>)
                 }
             </div>
             </div>
